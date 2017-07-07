1e8a384082dbaed6f182952bbbbce848cc872ab9
Sam <elucidation@gmail.com>
2017-07-07 00:15:54 -0700

    Fix to work during 24 hr check-in process

--- content.js
+++ content.js
@@ -1,12 +1,20 @@
 // Look for a DOM element with the plane name (class name spanEquipment)
 var plane_name_search = document.getElementsByClassName("spanEquipment");
+var alt_plane_search = document.getElementById('equipDisplay');
+
+var plane_name = null;
+var name_element = null;
 
 // If it's found, we'll modify it to link to a seatguru page
 if (plane_name_search.length == 1) {
-  var name_element =  plane_name_search[0];
+  name_element =  plane_name_search[0];
   plane_name = name_element.textContent;
-  console.log("Plane:", plane_name);
+} else if (alt_plane_search && alt_plane_search.childElementCount > 0) {
+  plane_name = alt_plane_search.firstElementChild.innerText;
+  name_element = alt_plane_search.firstElementChild;
+}
 
+if (plane_name) {
   // Generate new HTML that wraps the plane title in the seatmap
   // with a version that links to a google i'm feeling lucky search result
   // with United + that plane title
@@ -18,7 +26,4 @@ if (plane_name_search.length == 1) {
 
   // Finally, update element with the new html
   name_element.outerHTML = linked_html;
-
-} else {
-  // console.log("No plane element found");
 }
